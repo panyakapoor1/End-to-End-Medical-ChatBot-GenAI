9505ce836bd8da30d7ba3053880704b10ebbcbb4
Panya <panyakapoor1@users.noreply.github.com>
2025-03-05 19:11:23 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup # type: ignore
+
+setup(
+  name = 'Generative AI Project',
+  version= '0.0.0',
+  author='Panya Kapoor',
+  author_email='panyakapoor1@gmail.com',
+  packages= find_packages(),
+  install_requires = []
+)
